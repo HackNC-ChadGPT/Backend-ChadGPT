6edd4e2b47be1da04619bab1cd4093c2c8a450e7
bryanhce <ho.cheng.en.bryan@gmail.com>
2023-10-28 18:13:28 -0400

    add cross origin

--- bot.py
+++ bot.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import os
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/getConfidence')
+@app.route('/getConfidence', methods=['POST'])
+@cross_origin()
 def hello_world():
     # call backend stuff
-    return '42%'
+    try:
+        data = request.json
+        prompt = data.get('prompt')
+        if prompt is not None:
+            return jsonify({"result": "42.5%"})
+        else:
+            return jsonify({"error": "No 'prompt' data found in the request body"})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 9999))
