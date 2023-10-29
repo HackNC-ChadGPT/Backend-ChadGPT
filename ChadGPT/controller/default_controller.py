d9c4d3034b9ca481ac064fdd6762e18ea60b5000
Ho Cheng En Bryan <72288288+bryanhce@users.noreply.github.com>
2023-10-29 10:51:47 -0400

    Reduce number of jobs run

--- ChadGPT/controller/default_controller.py
+++ ChadGPT/controller/default_controller.py
@@ -6,7 +6,7 @@ from controller import Controller
 
 class DefaultController(Controller):
     def run(self, query):
-        answers = self.get_answers(query, 3)
+        answers = self.get_answers(query, 2)
         agreements = self.get_agreement(answers)
         print(f'\n=========\nAnswers \n{answers}\n{agreements}\n')
         return sum(agreements)/len(agreements)
